021d695194d67f68abe6e3dc65cd24bc285706b2
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-06-23 15:50:41 +0700

    selesai membuat int main

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -6,7 +6,7 @@ class Queues {
 	int queue_array[5];
 
 public:
-	Queue() {
+	Queues() {
 		FRONT = -1;
 		REAR = -1;
 	}
@@ -18,7 +18,7 @@ public:
 		cout << endl;
 
 		//cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1)) || (FRONT == REAR + 1) {
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
@@ -108,6 +108,37 @@ int main() {
 			cout << "2. Implement delete operation" << endl;
 			cout << "3. Display values" << endl;
 			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2':
+				q.remove();
+				break;
+			
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
+
+	return 0;
 }
